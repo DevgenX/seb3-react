9f2fbecc73107402825d8b5037905f08b9b40eac
Seb <107775878+DevgenX@users.noreply.github.com>
2023-07-06 18:53:48 -0700

    Delete metamask.d.ts

--- src/metamask.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  interface Window {
-    ethereum?: {
-      request: (args: { method: string }) => Promise<string[]>;
-      on?: (event: string, listener: (arg: any) => void) => void;
-      removeListener?: (event: string, listener: (arg: any) => void) => void;
-    };
-  }
-}
